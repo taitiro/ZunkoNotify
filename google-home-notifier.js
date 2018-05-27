fbd2678fcc6fb43698cce0b11241548e9b12686f
Taichirou Kosugi <kosugi@darakeru.com>
2018-05-28 08:11:34 +0900

    hoge

--- google-home-notifier.js
+++ google-home-notifier.js
@@ -75,13 +75,13 @@ var getSpeechUrl = function(text, host, callback) {
   };
 
   function callback(error, response, body) {
-    if (!error && typeof response.statusCode != undefined && response.statusCode == 200) {
+    if (!error && typeof response != undefined  && typeof response.statusCode != undefined && response.statusCode == 200) {
       console.log(body);
     }
   }
 
   request(options, function(error, response, body) {
-    if (!error && typeof response.statusCode != undefined &&response.statusCode == 200) {
+    if (!error && typeof response != undefined && typeof response.statusCode != undefined &&response.statusCode == 200) {
       console.log('Audio content written to file: ' + tempFileUrl);
       onDeviceUp(host, path + tempFileUrl, function(res) {
         callback(res)
