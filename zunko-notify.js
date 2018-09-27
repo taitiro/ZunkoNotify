3261c22238dae8bb4050636cb1bb8fd31c123e67
Taichirou Kosugi <kosugi@darakeru.com>
2018-09-28 00:19:21 +0900

    change order

--- zunko-notify.js
+++ zunko-notify.js
@@ -12,32 +12,6 @@ const init = (_localAddress, _zunkoAddress) =>  {
   zunkoAddress = _zunkoAddress;
 };
 
-const notify = (_ip, _message, _callback) => {
-  request.post({
-    url: zunkoAddress + 'SAVE/1700',
-    encoding: null,
-    headers: {
-      "Content-type": "application/json",
-    },
-    json: {
-      talktext: _message,
-      speed: 1.0,
-      volume:2.0,
-      pitch:1.0,
-      intonation:1.0
-    }
-  }, (_err, _res, _body) => {
-    if(_err){
-      console.error(_err);
-      callback(`ERROR: ${_err}`)
-    }else{
-      fs.writeFileSync('data/' + fileName, _body, 'binary');
-      play(_ip, localAddress + fileName , function(_res) {
-        _callback(_res)
-      });
-    }
-  });
-};
 
 const play = (_ip, _url, _callback) => {
   deviceAddressArray.forEach((_deviceAddress) => {  
@@ -63,6 +37,33 @@ const play = (_ip, _url, _callback) => {
       client.close();
       _callback('error');
     });
+  });
+};
+
+const notify = (_ip, _message, _callback) => {
+  request.post({
+    url: zunkoAddress + 'SAVE/1700',
+    encoding: null,
+    headers: {
+      "Content-type": "application/json",
+    },
+    json: {
+      talktext: _message,
+      speed: 1.0,
+      volume:2.0,
+      pitch:1.0,
+      intonation:1.0
+    }
+  }, (_err, _res, _body) => {
+    if(_err){
+      console.error(_err);
+      callback(`ERROR: ${_err}`)
+    }else{
+      fs.writeFileSync('data/' + fileName, _body, 'binary');
+      play(_ip, localAddress + fileName , function(_res) {
+        _callback(_res)
+      });
+    }
   });
 };
 
